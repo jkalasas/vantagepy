74a35e0307ecb0bc268bf744b2c4899572f027a0
jkalasas <alasasjohnkyle@gmail.com>
2022-08-24 22:24:39 +0800

    Added core stock API support

---
+++ pyvantage/__init__.py
@@ -0,0 +1,8 @@
+"""
+PyVantage
+---------
+
+PyVantage is a API client for Alpha Vantage
+"""
+
+from .client import *
---
+++ setup.py
@@ -0,0 +1,26 @@
+"""
+PyVantage
+---------
+
+PyVantage is a API client for Alpha Vantage
+"""
+
+from setuptools import setup
+
+setup(
+    name="PyVantage",
+    version="0.0.1",
+    url="https://github.com/jkalasas/pyvantage",
+    license="BSD",
+    author="John Kyle Alas-as",
+    author_email="alasasjohnkyle@gmail.com",
+    description="API client for Alpha Vantage",
+    long_description=__doc__,
+    packages=["pyvantage",],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        "requests",
+    ],
+)
